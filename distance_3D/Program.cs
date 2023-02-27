f5846fbf1de40cde4905f3e61c2d03a7acf7a9cf
Kubinic <Osiptsov84@gmail.com>
2023-02-27 14:44:27 +0700

    DZ_3

---
+++ distance_3D/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.d = (x1-x2)+(y1-y2)+(z1-z2)
+Console.WriteLine("Введите координаты точки 1 ");
+Console.Write("Оси x1 : ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.Write("Оси y1 : ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.Write("Оси z1 : ");
+double z1 = double.Parse(Console.ReadLine()!);
+
+double distance = 0;
+double resalt = 0;
+
+Console.WriteLine("Введите координаты точки 2 ");
+Console.Write("Оси x2 : ");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.Write("Оси y2 : ");
+double y2 = double.Parse(Console.ReadLine()!);
+Console.Write("Оси z2 : ");
+double z2 = double.Parse(Console.ReadLine()!);
+
+
+resalt = Math.Sqrt((Math.Pow((x2-x1), 2)+Math.Pow((y2-y1), 2)+Math.Pow((z2-z1), 2)));
+
+
+    
+Console.WriteLine($"{Math.Round(resalt, 2)}");
---
+++ exponentiation/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+//* при N < 0, нужно вывести от N до -1
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine()!);
+int exp = 3;
+int count = 0;
+int digit = 1;
+if(num > 0){
+    while (count <num){
+    Console.Write($"{Math.Pow(digit, exp)}, ");
+    digit++;
+    count++;
+    }
+}
+else
+    while(count < (num*(-1))){
+    Console.Write($"{(Math.Pow(digit, exp)*(-1))}, ");
+    digit++;
+    count++;
+    }
